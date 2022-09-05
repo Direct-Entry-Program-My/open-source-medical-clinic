ff5017c6dbe9e8e01a3f41b24884e24d8744c4ee
Eranga Bandara <erangabandara94@gmail.com>
2022-09-05 17:55:21 +0530

    Close #13

---
+++ src/main/java/lk/ijse/dep9/clinic/controller/AddModifyProfileFormController.java
@@ -0,0 +1,29 @@
+package lk.ijse.dep9.clinic.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class AddModifyProfileFormController {
+
+    public ComboBox cmbProfileType;
+    public TextField txtName;
+    public TextField txtUsername;
+    public ComboBox cmbField;
+    public ComboBox cmbGender;
+    public TextField txtNIC;
+    public TextField txtContact;
+    public TextField txtAddress;
+    public TextField txtPassword;
+    public JFXButton btnSave;
+    public JFXButton btnReset;
+
+    public void btnResetOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+
+    }
+}
