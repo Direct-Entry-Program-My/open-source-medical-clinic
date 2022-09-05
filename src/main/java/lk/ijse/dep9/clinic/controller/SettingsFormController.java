c795a0bd338a1e5ba01b84e415ea2f01c7199d53
Eranga Bandara <erangabandara94@gmail.com>
2022-09-05 17:17:20 +0530

    Close #8

---
+++ src/main/java/lk/ijse/dep9/clinic/controller/SettingsFormController.java
@@ -0,0 +1,35 @@
+package lk.ijse.dep9.clinic.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class SettingsFormController {
+
+    public JFXButton btnHospitalFee;
+    public JFXButton btnDiscount;
+    public JFXButton btnManageFields;
+    public JFXButton btnPassword;
+    public JFXButton btnAbout;
+    public AnchorPane pneSettings;
+
+    public void btnHospitalFeeOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnDiscountOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnManageFieldsOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnPasswordOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnAboutOnAction(ActionEvent actionEvent) {
+
+    }
+}
