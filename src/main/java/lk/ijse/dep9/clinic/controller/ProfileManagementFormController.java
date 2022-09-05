9738a08f01ffcb7ad807c42f78bde2932198674f
Eranga Bandara <erangabandara94@gmail.com>
2022-09-05 17:29:12 +0530

    Close #9

---
+++ src/main/java/lk/ijse/dep9/clinic/controller/ProfileManagementFormController.java
@@ -0,0 +1,25 @@
+package lk.ijse.dep9.clinic.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+
+public class ProfileManagementFormController {
+
+    public TableView tblProfiles;
+    public JFXButton btnAdd;
+    public JFXButton btnModify;
+    public JFXButton btnDelete;
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnAddOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnModifyOnAction(ActionEvent actionEvent) {
+
+    }
+}
