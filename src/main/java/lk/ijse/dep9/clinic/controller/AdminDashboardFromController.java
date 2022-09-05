e41646303ce2a7e0213ed46237e4a2e2581137f5
Eranga Bandara <erangabandara94@gmail.com>
2022-09-05 16:59:34 +0530

    Close #7

--- src/main/java/lk/ijse/dep9/clinic/controller/AdminDashboardFromController.java
+++ src/main/java/lk/ijse/dep9/clinic/controller/AdminDashboardFromController.java
@@ -1,11 +1,33 @@
 package lk.ijse.dep9.clinic.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
 import lk.ijse.dep9.clinic.security.SecurityContextHolder;
 
 public class AdminDashboardFromController {
+
+    public JFXButton btnProfileManage;
+    public JFXButton btnViewRecords;
+    public JFXButton btnSettings;
+    public JFXButton btnLogOut;
 
     public void initialize(){
         System.out.println(SecurityContextHolder.getPrincipal());
     }
 
+    public void btnProfileManageOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnViewRecordsOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnSettingsOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnLogOutOnAction(ActionEvent actionEvent) {
+
+    }
 }
